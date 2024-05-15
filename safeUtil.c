82556ce1c4e224f4634cdc43d50ab19847f05fa4
arh <arh@monkeypeppershoe.com>
2024-05-15 12:00:10 -0700

    casting print to char

--- safeUtil.c
+++ safeUtil.c
@@ -24,7 +24,7 @@ int safeRecvfrom(int socketNum, void * buf, int len, int flags, struct sockaddr 
 		perror("recvfrom: ");
 		exit(-1);
 	}
-	printf("safeRecvfrom payload: %s\n", buf + 7);
+	printf("safeRecvfrom payload: %s\n", (char *)(buf + (sizeof(uint8_t) * 7)));
 	return returnValue;
 }
 
